30979fbe17a3fd3d477c5ec5225418772c4a3ab2
Jan Vornberger <jan@uos.de>
2014-04-23 23:08:19 +0200

    Add query tests

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 "use strict"
 
-var http = require('http')
-var router = require('router')
-var route = router()
+var http = require('http');
+var router = require('router');
+var url = require('url');
 
+var route = router();
 route.get('/test1', function(req, res) {
     res.writeHead(200);
     res.end('Successful');
@@ -20,7 +21,31 @@ route.get('/test3', function(req, res) {
     res.end('{"result":"Successful"}');
 });
 
+route.get('/test4', function(req, res) {
+    var urlParts = url.parse(req.url, true);
+    var query = urlParts.query;
+
+    res.writeHead(200);
+
+    if (query['arg0'] === 'zero' && query['arg1'] == 1)
+        res.end('Successful');
+    else
+        res.end('Failed');
+});
+
+route.post('/test5', function(req, res) {
+    var urlParts = url.parse(req.url, true);
+    var query = urlParts.query;
+
+    res.writeHead(200);
+
+    if (query['arg0'] === 'zero' && query['arg1'] == 1)
+        res.end('Successful');
+    else
+        res.end('Failed');
+});
+
 var server = http.createServer(route);
 server.listen(process.env.PORT || 9009, function() {
-    console.info('server listening on http://localhost:' + server.address().port)
+    console.info('server listening on http://localhost:' + server.address().port);
 })
